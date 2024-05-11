478d6371ab76caa8dbfb1778782c01b68e00ac79
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-05-11 00:08:27 +0000

    feat: hide server info by default

--- client/src/components/home/server-info.tsx
+++ client/src/components/home/server-info.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ConnectionIDs } from '../../../../types/socket';
 import TextInput from '../test/text-input';
 
@@ -8,46 +9,56 @@ type Params = {
 };
 
 export default function ServerInfo({ server, setServer, connections }: Params) {
+	const [collapsed, setCollapsed] = useState(true);
+	const icon = collapsed ? 'bi-caret-down-fill' : 'bi-caret-up-fill';
+
 	return (
 		<div className='container'>
-			<h2>Server Info</h2>
-			<TextInput id='server' label='Server URL:' value={server} setValue={setServer} />
-			<div className='row'>
-				<div className='col'>
-					<h5>Clients</h5>
-					<table className='table col'>
-						<thead>
-							<th>#</th>
-							<th>ID</th>
-						</thead>
-						<tbody>
-							{connections &&
-								connections.clients.map((client, index) => (
-									<tr>
-										<th>{index}</th>
-										<td>{client}</td>
-									</tr>
-								))}
-						</tbody>
-					</table>
-				</div>
-				<div className='col'>
-					<h5>Workers</h5>
-					<table className='table col'>
-						<thead>
-							<th>#</th>
-							<th>ID</th>
-						</thead>
-						<tbody>
-							{connections &&
-								connections.workers.map((worker, index) => (
-									<tr>
-										<th>{index}</th>
-										<td>{worker}</td>
-									</tr>
-								))}
-						</tbody>
-					</table>
+			<div className='d-flex'>
+				<h2>Server Info</h2>
+				<button className='btn btn-link' onClick={() => setCollapsed(!collapsed)}>
+					<i className={`bi ${icon}`} />
+				</button>
+			</div>
+			<div className='card card-body' hidden={collapsed}>
+				<TextInput id='server' label='Server URL:' value={server} setValue={setServer} />
+				<div className='row'>
+					<div className='col'>
+						<h5>Clients</h5>
+						<table className='table col'>
+							<thead>
+								<th>#</th>
+								<th>ID</th>
+							</thead>
+							<tbody>
+								{connections &&
+									connections.clients.map((client, index) => (
+										<tr>
+											<th>{index}</th>
+											<td>{client}</td>
+										</tr>
+									))}
+							</tbody>
+						</table>
+					</div>
+					<div className='col'>
+						<h5>Workers</h5>
+						<table className='table col'>
+							<thead>
+								<th>#</th>
+								<th>ID</th>
+							</thead>
+							<tbody>
+								{connections &&
+									connections.workers.map((worker, index) => (
+										<tr>
+											<th>{index}</th>
+											<td>{worker}</td>
+										</tr>
+									))}
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
