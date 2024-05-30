151980c565e1d6d7ea84675a5a8b8526b4004af7
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-05-30 23:30:10 +0000

    refactor: connection typing structure change

--- types/socket.ts
+++ types/socket.ts
@@ -4,15 +4,19 @@ export type Client = Socket;
 
 export type Worker = Socket;
 
+export type ClientID = string;
+
+export type workerID = {
+	workerID: string;
+	connectionID: string;
+};
+
 export type Connections = {
 	clients: Client[];
 	workers: Worker[];
 };
 
 export type ConnectionIDs = {
-	clients: string[];
-	workers: {
-		workerID: string;
-		connectionID: string;
-	}[];
+	clients: ClientID[];
+	workers: workerID[];
 };
