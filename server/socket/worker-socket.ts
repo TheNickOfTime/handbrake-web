9c0b1972fcf40ea01d0855bfbf9f26f0bbc3087a
Nick <thenickoftime.x@gmail.com>
2024-06-18 21:58:44 +0000

    feat: set job to stopped if it's worker disconnects without warning

--- server/socket/worker-socket.ts
+++ server/socket/worker-socket.ts
@@ -1,8 +1,8 @@
 import { Server } from 'socket.io';
 import { AddWorker, RemoveWorker } from '../scripts/connections';
 import { TranscodeStage, TranscodeStatusUpdate } from '../../types/transcode';
-import { UpdateJob } from '../scripts/queue';
-import { UpdateJobInDatabase } from '../scripts/database/database-queue';
+import { GetQueue, StopJob, UpdateJob } from '../scripts/queue';
+import { GetJobFromDatabase, UpdateJobInDatabase } from '../scripts/database/database-queue';
 
 export default function WorkerSocket(io: Server) {
 	io.of('/worker').on('connection', (socket) => {
@@ -14,6 +14,18 @@ export default function WorkerSocket(io: Server) {
 		socket.on('disconnect', () => {
 			console.log(`[server] Worker '${workerID}' with ID '${socket.id}' has disconnected.`);
 			RemoveWorker(socket);
+			const queue = GetQueue();
+			if (queue) {
+				const workersJob = Object.keys(queue).find(
+					(jobID) => queue[jobID].worker == workerID
+				);
+				if (workersJob) {
+					StopJob(workersJob);
+					console.log(
+						`[server] Disconnected worker '${workerID}' was working on job '${workersJob}' when disconnected - setting job to stopped.`
+					);
+				}
+			}
 		});
 
 		socket.on('transcode-stopped', (status: TranscodeStatusUpdate) => {
