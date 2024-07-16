ac3ac590ffd9c8138b15012fa4819d9e5fa31c45
Nick <thenickoftime.x@gmail.com>
2024-07-16 04:52:57 +0000

    feat: now check if the config file exists, if not then copy a template config file

--- server/scripts/config.ts
+++ server/scripts/config.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
+import path from 'path';
 import { parse } from 'yaml';
 import { Config, ConfigProperty } from 'types/config';
+import { dataPath } from './data';
 
 let config: Config = {
 	'input-path': '/video',
@@ -8,11 +10,19 @@ let config: Config = {
 };
 
 export function LoadConfig() {
-	const configPath = '/workspaces/handbrake-web/config.yaml';
+	const configPath = path.join(dataPath, 'config.yaml');
+
+	if (!fs.existsSync(configPath)) {
+		console.log(`[server] [config] No config file exists, copying the template config.yaml`);
+		fs.copyFileSync(path.resolve('template/config.yaml'), configPath);
+	}
+
 	const configFile = fs.readFileSync(configPath, 'utf-8');
 	const configData = parse(configFile);
 	config = configData;
 }
+
+function CreateConfig() {}
 
 export function GetConfig() {
 	return config;
