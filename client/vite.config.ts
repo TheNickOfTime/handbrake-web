b1e07944f2230ee466d1949f91298f33479e41bb
Nick <thenickoftime.x@gmail.com>
2024-07-20 01:10:48 +0000

    fix: attempting to fix linting

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
-	server: {
-		host: '127.0.0.1',
-		port: process.env.CLIENT_PORT ? parseInt(process.env.CLIENT_PORT) : 5173,
-	},
 	build: {
 		outDir: './build',
 		emptyOutDir: true,
+	},
+	plugins: [react(), tsconfigPaths()],
+	server: {
+		host: '127.0.0.1',
+		port: 5173,
 	},
 });
