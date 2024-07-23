815de62f49ca2bb19969ca0214be046b9e93cacc
Nick <thenickoftime.x@gmail.com>
2024-07-23 01:59:03 +0000

    fix: setup vite to resolve path aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,14 @@ export default defineConfig({
 		emptyOutDir: true,
 	},
 	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			components: path.resolve(__dirname, './src/components'),
+			pages: path.resolve(__dirname, './src/pages'),
+			sections: path.resolve(__dirname, './src/sections'),
+			types: path.resolve(__dirname, '../types/src'),
+		},
+	},
 	server: {
 		host: '127.0.0.1',
 		port: 5173,
