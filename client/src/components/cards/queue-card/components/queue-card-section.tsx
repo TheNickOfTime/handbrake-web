c1bb085dba0d35336b0db3cd5fbc9d28f7ff1966
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-07-23 19:28:21 -0700

    feat: refactor queue-job component into queue-card and update the display of certain information fields (#75)
    * refactor: refactoring queue-job.tsx into queue-card.tsx
    
    * feat: add 'title' property to queu-card-section that adds an info icon for additional info
    
    * feat: adding input section and title properties to the queue-card

--- client/src/components/cards/queue-card/components/queue-card-section.tsx
+++ client/src/components/cards/queue-card/components/queue-card-section.tsx
@@ -1,14 +1,17 @@
 import { PropsWithChildren } from 'react';
-import './queue-job-section.scss';
 
 type Params = PropsWithChildren & {
 	label: string;
+	title?: string;
 };
 
-export default function QueueJobSection({ children, label }: Params) {
+export default function QueueCardSection({ children, label, title }: Params) {
 	return (
 		<div className={`job-section`} id={label.toLowerCase().replace(/\s/g, '-')}>
-			<h5 className='job-section-label'>{label}</h5>
+			<h5 className='job-section-label'>
+				<span>{label}</span>
+				{title && <i className='bi bi-info-circle-fill' title={title} />}
+			</h5>
 			<hr />
 			<div className='job-section-children'>{children}</div>
 		</div>
