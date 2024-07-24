f2183c596323ce87cca5959fc2d4111ad8d90eab
Nick <thenickoftime.x@gmail.com>
2024-07-24 02:19:47 +0000

    feat: add 'title' property to queu-card-section that adds an info icon for additional info

--- client/src/components/cards/queue-card/components/queue-card-section.tsx
+++ client/src/components/cards/queue-card/components/queue-card-section.tsx
@@ -2,12 +2,16 @@ import { PropsWithChildren } from 'react';
 
 type Params = PropsWithChildren & {
 	label: string;
+	title?: string;
 };
 
-export default function QueueCardSection({ children, label }: Params) {
+export default function QueueCardSection({ children, label, title }: Params) {
 	return (
 		<div className={`job-section`} id={label.toLowerCase().replace(/\s/g, '-')}>
-			<h5 className='job-section-label'>{label}</h5>
+			<h5 className='job-section-label'>
+				<span>{label}</span>
+				{title && <i className='bi bi-info-circle-fill' title={title} />}
+			</h5>
 			<hr />
 			<div className='job-section-children'>{children}</div>
 		</div>
