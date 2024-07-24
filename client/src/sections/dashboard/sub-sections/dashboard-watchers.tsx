b296a3c354faec17977d44a42840b8787aacbf15
Nick <thenickoftime.x@gmail.com>
2024-07-24 02:37:25 +0000

    feat: add watcher section to the dashboard

---
+++ client/src/sections/dashboard/sub-sections/dashboard-watchers.tsx
@@ -0,0 +1,39 @@
+import SubSection from 'components/section/sub-section';
+import { NavLink } from 'react-router-dom';
+import { WatcherDefinitionWithIDType } from 'types/watcher';
+
+type Params = {
+	watchers: WatcherDefinitionWithIDType[];
+};
+
+export default function DashboardWatchers({ watchers }: Params) {
+	return (
+		<SubSection id='watchers'>
+			<NavLink to='/watchers'>
+				<h2>
+					Watchers <i className='bi bi-arrow-right-short' />
+				</h2>
+			</NavLink>
+			<div className='table-scroll'>
+				<table>
+					<thead>
+						<tr>
+							<th>Watching Directory</th>
+							<th>Output Directory</th>
+							<th>Preset</th>
+						</tr>
+					</thead>
+					<tbody>
+						{watchers.map((watcher) => (
+							<tr>
+								<td>{watcher.watch_path}</td>
+								<td>{watcher.output_path || 'Same as watching'}</td>
+								<td className='center'>{watcher.preset_id}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</SubSection>
+	);
+}
