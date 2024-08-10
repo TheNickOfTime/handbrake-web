76884d541d26e99be276782f176f0a6fb3845a00
Nick <thenickoftime.x@gmail.com>
2024-08-10 22:58:59 +0000

    feat: adding empty settings component and sidebar link/routing to it in the client

---
+++ client/src/sections/settings/settings.tsx
@@ -0,0 +1,6 @@
+import Section from 'components/section/section';
+import './settings.scss';
+
+export default function SettingsSection() {
+	return <Section title='Settings' id='settings-section'></Section>;
+}
