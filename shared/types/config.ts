628b5724cead21a1ab35ff825c628e7578a3f6c8
Nick <thenickoftime.x@gmail.com>
2024-08-11 22:58:30 +0000

    feat: updating config template with new layout and additional values

--- shared/types/config.ts
+++ shared/types/config.ts
@@ -1,6 +1,17 @@
 export type ConfigType = {
+	paths: ConfigPathsType;
+	presets: ConfigPresetsType;
+};
+
+export type ConfigPathsType = {
+	'media-path': string;
 	'input-path': string;
 	'output-path': string;
+};
+
+export type ConfigPresetsType = {
+	'show-default-presets': boolean;
+	'allow-preset-creator': boolean;
 };
 
 export type ConfigPropertyType = keyof ConfigType;
