f570719c227b3a49096c053967defb9991a046a8
Nick <thenickoftime.x@gmail.com>
2024-08-13 01:52:26 +0000

    feat: adding auto-fix config option to the settings section

--- client/src/sections/settings/settings.tsx
+++ client/src/sections/settings/settings.tsx
@@ -7,6 +7,7 @@ import SubSection from 'components/section/sub-section';
 import ButtonInput from 'components/base/inputs/button/button-input';
 import SettingsPaths from './sub-sections/settings-paths';
 import SettingsPreset from './sub-sections/settings-presets';
+import ToggleInput from 'components/base/inputs/toggle/toggle-input';
 
 export default function SettingsSection() {
 	const { config, socket } = useOutletContext<PrimaryOutletContextType>();
@@ -22,17 +23,30 @@ export default function SettingsSection() {
 		socket.emit('config-update', currentConfig);
 	};
 
+	const handleAutoFixChange = (value: boolean) => {
+		setCurrentConfig({
+			...currentConfig,
+			config: {
+				'auto-fix': value,
+			},
+		});
+	};
+
 	return (
 		<Section title='Settings' id='settings-section'>
 			<SubSection id='buttons'>
-				<div>
-					<ButtonInput
-						label='Save Configuration'
-						icon='bi-floppy2-fill'
-						onClick={handleSaveConfig}
-						disabled={!canSave}
-					/>
-				</div>
+				<ButtonInput
+					label='Save Configuration'
+					icon='bi-floppy2-fill'
+					onClick={handleSaveConfig}
+					disabled={!canSave}
+				/>
+				<ToggleInput
+					id='auto-fix-toggle'
+					label='Auto-fix Configuration Errors (recommended)'
+					value={currentConfig.config['auto-fix']}
+					onChange={handleAutoFixChange}
+				/>
 			</SubSection>
 			<div className='settings-sub-sections'>
 				<SettingsPaths config={currentConfig} setConfig={setCurrentConfig} />
