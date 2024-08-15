f2ae285c5cf6de0852f815c4c9c92f07c40b11a2
Nick <thenickoftime.x@gmail.com>
2024-08-15 19:58:17 +0000

    feat: add OnShutdown function to have a common shutdown process between different signals

--- server/src/scripts/shutdown.ts
+++ server/src/scripts/shutdown.ts
@@ -1,13 +1,23 @@
+import { DatabaseDisconnect } from './database/database';
+
 export default function Shutdown() {
 	process.on('SIGINT', (signal) => {
 		console.log(
-			'[server] The process has been interrupted, HandBrake Web will now shutdown...'
+			'[server] [shutdown] The process has been interrupted, HandBrake Web will now begin to shutdown...'
 		);
-		process.exit(0);
+		OnShutdown();
 	});
 
 	process.on('SIGTERM', (signal) => {
-		console.log('[server] The process has been terminated, HandBrake Web will now shutdown...');
-		process.exit(0);
+		console.log(
+			'[server] [shutdown] The process has been terminated, HandBrake Web will now begin to shutdown...'
+		);
+		OnShutdown();
 	});
+}
+
+function OnShutdown() {
+	DatabaseDisconnect();
+	console.log(`[server] [shutdown] Shutdown steps have completed.`);
+	process.exit();
 }
