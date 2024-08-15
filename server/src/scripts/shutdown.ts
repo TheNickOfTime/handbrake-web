e8197079a3b80649e2f068a9b2a056c961f2574d
Nick <thenickoftime.x@gmail.com>
2024-08-15 21:06:44 +0000

    feat: adding shutdown steps before exiting the process

--- server/src/scripts/shutdown.ts
+++ server/src/scripts/shutdown.ts
@@ -1,23 +1,40 @@
+import { Server } from 'node:http';
+import { Server as SocketServer } from 'socket.io';
+
 import { DatabaseDisconnect } from './database/database';
 
-export default function Shutdown() {
-	process.on('SIGINT', (signal) => {
+export default function Shutdown(socket: SocketServer) {
+	process.on('SIGINT', () => {
 		console.log(
-			'[server] [shutdown] The process has been interrupted, HandBrake Web will now begin to shutdown...'
+			`[server] [shutdown] The process has been interrupted, HandBrake Web will now begin to shutdown...`
 		);
-		OnShutdown();
+		OnShutdown(socket);
 	});
 
-	process.on('SIGTERM', (signal) => {
+	process.on('SIGTERM', () => {
 		console.log(
-			'[server] [shutdown] The process has been terminated, HandBrake Web will now begin to shutdown...'
+			`[server] [shutdown] The process has been terminated, HandBrake Web will now begin to shutdown...`
 		);
-		OnShutdown();
+		OnShutdown(socket);
 	});
 }
 
-function OnShutdown() {
-	DatabaseDisconnect();
-	console.log(`[server] [shutdown] Shutdown steps have completed.`);
-	process.exit();
+async function OnShutdown(socket: SocketServer) {
+	try {
+		// Shutdown the socket server
+		await new Promise<void>((resolve) => {
+			socket.close((err) => {
+				if (err) throw err;
+				resolve();
+			});
+		});
+
+		await DatabaseDisconnect();
+		console.log(`[server] [shutdown] Shutdown steps have completed.`);
+	} catch (error) {
+		console.error(`[server] [shutdown] [error] Could not complete shutdown steps.`);
+		console.error(error);
+	}
+
+	process.exit(0);
 }
