ce8b7711957e858987fcd76e20033f60a21c72a4
Nick <thenickoftime.x@gmail.com>
2024-08-15 01:24:50 +0000

    feat: add support for running database migrations when database_version upgrades

--- server/src/scripts/database/database.ts
+++ server/src/scripts/database/database.ts
@@ -4,8 +4,11 @@ import path from 'path';
 import { dataPath } from '../data';
 import { watcherTableCreateStatements } from 'scripts/database/database-watcher';
 import { queueTableCreateStatements } from './database-queue';
+import DatabaseMigrations from './migrations/database-migrations';
 
 const databasePath = path.join(dataPath, 'handbrake.db');
+
+export const databaseVersion = 1;
 
 export let database: DatabaseType = new Database(databasePath, {});
 database.pragma('journal_mode = WAL');
@@ -13,6 +16,14 @@ database.pragma('foreign_keys = ON');
 
 export function DatabaseConnect() {
 	try {
+		// Check if database tables exist ----------------------------------------------------------
+		const checkTablesStatement = database.prepare(
+			`SELECT name FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%'`
+		);
+		const checkTablesResult = checkTablesStatement.all();
+		const databaseExists = checkTablesResult.length > 0;
+
+		// Create the tables if they don't exist ---------------------------------------------------
 		const tableCreateStatements = [
 			...queueTableCreateStatements,
 			'CREATE TABLE IF NOT EXISTS status(id TEXT NOT NULL, state INTEGER NOT NULL, PRIMARY KEY (id))',
@@ -21,10 +32,51 @@ export function DatabaseConnect() {
 			.map((statement) => statement.replace(/\t/gm, ''))
 			.map((statement) => database.prepare(statement));
 
-		// Create the tables if they don't exist
 		tableCreateStatements.forEach((statement) => statement.run());
+
+		// Check database version ------------------------------------------------------------------
+		CheckDatabaseVersion(databaseExists);
+
 		console.log('[server] [database] The database connection has been initalized!');
 	} catch (err) {
 		console.error(err);
+	}
+}
+
+function CheckDatabaseVersion(databaseExists: boolean) {
+	const createVersionTableStatement = database.prepare(
+		'CREATE TABLE IF NOT EXISTS database_version(version INT NOT NULL PRIMARY KEY)'
+	);
+	if (databaseExists) {
+		try {
+			const checkVersionStatement = database.prepare<[], { version: number }>(
+				'SELECT version FROM database_version'
+			);
+			const checkVersionResult = checkVersionStatement.get();
+			if (!checkVersionResult) {
+				throw new Error(
+					'[server] [database] [error] Cannot get database_version from the database. Application will now shut down.'
+				);
+			}
+
+			const currentVersion = checkVersionResult.version;
+
+			if (currentVersion < databaseVersion) {
+				DatabaseMigrations(currentVersion);
+			} else if (currentVersion > databaseVersion) {
+				console.error(
+					`[server] [database] [error] The database's version is greater than the application's target version. The database cannot be downgraded and therefore cannot be loaded.`
+				);
+			}
+		} catch {
+			DatabaseMigrations(0);
+		}
+	} else {
+		createVersionTableStatement.run();
+
+		const insertVersionStatement = database.prepare<{ version: number }>(
+			'INSERT INTO database_version(version) VALUES($version)'
+		);
+		insertVersionStatement.run({ version: databaseVersion });
 	}
 }
---
+++ server/src/scripts/database/migrations/database-migrations.ts
@@ -0,0 +1,43 @@
+import { Database } from 'better-sqlite3';
+import { database, databaseVersion } from '../database';
+import DatabaseMigration0 from './database-migration-0';
+// import DatabaseMigration1 from './database-migration-1';
+
+export default function DatabaseMigrations(version: number) {
+	try {
+		console.log(
+			`[server] [database] [migration] The database_version is out of date, performing migrations...`
+		);
+		for (let i = version; i <= databaseVersion; i++) {
+			RunDatabaseMigration(i);
+		}
+		console.log(
+			`[server] [database] [migrations] Database migrations have completed, the database_version is up to date.`
+		);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function RunDatabaseMigration(version: number) {
+	console.log(
+		`[server] [database] [migration] Running migration script for database_version ${version}...`
+	);
+
+	switch (version) {
+		case 0:
+			DatabaseMigration0(database);
+			break;
+		// case 1:
+		// 	DatabaseMigration1(database);
+		// 	break;
+	}
+
+	const upgradeVersionStatement = database.prepare<{ version: number }>(
+		'UPDATE database_version SET version = $version'
+	);
+	upgradeVersionStatement.run({ version: version });
+	// console.log(
+	// 	`[server] [database] [migration] The database_version has been upgraded to version ${version}.`
+	// );
+}
