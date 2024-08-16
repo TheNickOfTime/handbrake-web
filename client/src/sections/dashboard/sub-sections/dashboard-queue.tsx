8a014275f216461589d6eae7c116cdf2ff17f1a7
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-08-16 14:16:17 -0700

    feat: update job sorting for the queue section of the dashboard (#147)

--- client/src/sections/dashboard/sub-sections/dashboard-queue.tsx
+++ client/src/sections/dashboard/sub-sections/dashboard-queue.tsx
@@ -10,6 +10,14 @@ type Params = {
 	queue: QueueType;
 };
 
+const statusSorting: { [key in TranscodeStage]: number } = {
+	[TranscodeStage.Transcoding]: 1,
+	[TranscodeStage.Scanning]: 2,
+	[TranscodeStage.Waiting]: 3,
+	[TranscodeStage.Stopped]: 4,
+	[TranscodeStage.Finished]: 5,
+};
+
 export default function DashboardQueue({ queue }: Params) {
 	return (
 		<SubSection id='queue'>
@@ -22,6 +30,7 @@ export default function DashboardQueue({ queue }: Params) {
 				<table>
 					<thead>
 						<tr>
+							<th>#</th>
 							<th>Input</th>
 							<th>Output</th>
 							<th>Status</th>
@@ -29,52 +38,94 @@ export default function DashboardQueue({ queue }: Params) {
 						</tr>
 					</thead>
 					<tbody>
-						{Object.keys(queue).map((key) => {
-							const job = queue[key];
-							const percentage = job.status.transcode_percentage
-								? job.status.transcode_percentage * 100
-								: 0;
+						{Object.keys(queue)
+							.sort(
+								(a, b) => {
+									const stageA = queue[a].status.transcode_stage;
+									const stageB = queue[b].status.transcode_stage;
+									if (stageA != undefined && stageB != undefined) {
+										// return (
+										// 	statusSorting[queue[a].status.transcode_stage] -
+										// 	statusSorting[queue[b].status.transcode_stage]
+										// );
+										const orderA = queue[a].order_index;
+										const orderB = queue[b].order_index;
 
-							return (
-								<tr key={`queue-job-${key}`}>
-									<td className='input' title={job.data.input_path}>
-										{job.data.input_path.match(/[^/]+$/)}
-										<BadgeInfo info={job.data.input_path} />
-									</td>
-									<td className='output' title={job.data.output_path}>
-										{job.data.output_path.match(/[^/]+$/)}
-										<BadgeInfo info={job.data.output_path} />
-									</td>
-									<td
-										className={`status center ${
-											job.status.transcode_stage == TranscodeStage.Waiting
-												? 'color-yellow'
-												: job.status.transcode_stage ==
-												  TranscodeStage.Scanning
-												? 'color-orange'
-												: job.status.transcode_stage ==
-												  TranscodeStage.Transcoding
-												? 'color-blue'
-												: job.status.transcode_stage ==
-												  TranscodeStage.Finished
-												? 'color-green'
-												: ''
-										}`}
-									>
-										{
-											TranscodeStage[
-												job.status.transcode_stage
-													? job.status.transcode_stage
-													: 0
-											]
-										}
-									</td>
-									<td className='progress'>
-										<ProgressBar percentage={percentage} />
-									</td>
-								</tr>
-							);
-						})}
+										const finishedA = queue[a].status.time_finished || 0;
+										const finishedB = queue[b].status.time_finished || 0;
+
+										return stageA == stageB
+											? orderA != null && orderB != null
+												? orderA - orderB
+												: finishedA
+												? finishedB
+													? finishedB - finishedA
+													: 1
+												: finishedB
+												? -1
+												: 0
+											: statusSorting[stageA] - statusSorting[stageB];
+									}
+
+									return 0;
+								}
+								// queue[a].order_index
+								// 	? queue[b].order_index
+								// 		? queue[a].order_index - queue[b].order_index
+								// 		: -1
+								// 	: queue[b].order_index
+								// 	? 1
+								// 	: 1
+							)
+							.map((key) => {
+								const job = queue[key];
+								const percentage = job.status.transcode_percentage
+									? job.status.transcode_percentage * 100
+									: 0;
+
+								// console.log(job.order_index);
+
+								return (
+									<tr key={`queue-job-${key}`}>
+										<td className='order center'>{job.order_index}</td>
+										<td className='input' title={job.data.input_path}>
+											{job.data.input_path.match(/[^/]+$/)}
+											<BadgeInfo info={job.data.input_path} />
+										</td>
+										<td className='output' title={job.data.output_path}>
+											{job.data.output_path.match(/[^/]+$/)}
+											<BadgeInfo info={job.data.output_path} />
+										</td>
+										<td
+											className={`status center ${
+												job.status.transcode_stage == TranscodeStage.Waiting
+													? 'color-yellow'
+													: job.status.transcode_stage ==
+													  TranscodeStage.Scanning
+													? 'color-orange'
+													: job.status.transcode_stage ==
+													  TranscodeStage.Transcoding
+													? 'color-blue'
+													: job.status.transcode_stage ==
+													  TranscodeStage.Finished
+													? 'color-green'
+													: ''
+											}`}
+										>
+											{
+												TranscodeStage[
+													job.status.transcode_stage
+														? job.status.transcode_stage
+														: 0
+												]
+											}
+										</td>
+										<td className='progress'>
+											<ProgressBar percentage={percentage} />
+										</td>
+									</tr>
+								);
+							})}
 					</tbody>
 				</table>
 			</div>
