fb635c5e56fad49161a9a34ea8f925b0c8873afc
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2024-08-18 17:27:54 -0700

    feat: check critical file permissions prior to actually starting up the application/importing anything, failure results in immediate shutdown (#157)
    * feat: add function to check permissions of the data directory
    
    * feat: refactor application startup scripts to not load anything until permissions have been checked

--- server/src/scripts/data.ts
+++ server/src/scripts/data.ts
@@ -1,28 +1,58 @@
-import { readFile, writeFile } from 'fs/promises';
-import logger from 'logging';
+import { access, constants } from 'fs/promises';
 import path from 'path';
 
 export const dataPath = process.env.DATA_PATH || path.join(__dirname, '../../data');
 
-export async function WriteDataToFile(path: string, data: object) {
+export async function CheckDataDirectoryPermissions() {
 	try {
-		const json = JSON.stringify(data, undefined, '\t');
-		await writeFile(path, json, { encoding: 'utf-8' });
-		logger.info(`[server] [data] Data successfully written to file '${path}'`);
-	} catch (err) {
-		logger.error(`[server] [data] [error] Unable to write data to '${path}'`);
-		logger.error(err);
-	}
-}
+		let error = false;
 
-export async function ReadDataFromFile(path: string) {
-	try {
-		const json = await readFile(path, { encoding: 'utf-8' });
-		const data = await JSON.parse(json);
-		logger.info(`[server] [data] Successfully read data from '${path}'`);
-		return data;
-	} catch (err) {
-		logger.error(`[server] [data] [error] Could not read data from '${path}'.`);
-		logger.error(err);
+		// Check read permissions
+		try {
+			await access(dataPath, constants.R_OK);
+		} catch (err) {
+			console.error(
+				`\x1b[2m${new Date(Date.now()).toLocaleTimeString('en-US', {
+					hour12: false,
+				})}\x1B[22m \x1b[31m[data] [error]\x1B[39m The application does not have read permissions for the directory '${dataPath}':`
+			);
+			console.error(err);
+			error = true;
+		}
+
+		try {
+			await access(dataPath, constants.W_OK);
+		} catch (err) {
+			console.error(
+				`\x1b[2m${new Date(Date.now()).toLocaleTimeString('en-US', {
+					hour12: false,
+				})}\x1B[22m \x1b[31m[data] [error]\x1B[39m The application does not have write permissions for the directory '${dataPath}':`
+			);
+			console.error(err);
+			error = true;
+		}
+
+		if (error) {
+			throw new Error();
+		}
+	} catch (error) {
+		// shut down application
+		console.error(
+			`\x1b[2m${new Date(Date.now()).toLocaleTimeString('en-US', {
+				hour12: false,
+			})}\x1B[22m \x1b[31m[data] [error]\x1B[39m The application does not have adequate permissions for '${dataPath}'.`
+		);
+		console.error(
+			`\x1b[2m${new Date(Date.now()).toLocaleTimeString('en-US', {
+				hour12: false,
+			})}\x1B[22m \x1b[31m[data] [error]\x1B[39m Did you create the directories you mapped in the docker compose file prior to the creation of the container?\n\tIf not, docker creates these directories for you with root permissions.\n\tPlease run 'chown' or otherwise modify permissions to have read & write access for the user you are running this container as.`
+		);
+		console.error(
+			`\x1b[2m${new Date(Date.now()).toLocaleTimeString('en-US', {
+				hour12: false,
+			})}\x1B[22m \x1b[31m[data] [error]\x1B[39m The application cannot run without proper permissions for the data folder. The application will now shutdown.`
+		);
+
+		process.exit(0);
 	}
 }
