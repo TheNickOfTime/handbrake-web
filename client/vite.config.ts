5b6abbd23c6d74c209fb19ef7a056f85832f1f01
Nick <thenickoftime.x@gmail.com>
2024-08-20 01:22:24 +0000

    fix: dead dotenv import

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
-import 'dotenv/config';
 import { env } from 'process';
 
 // https://vitejs.dev/config/
