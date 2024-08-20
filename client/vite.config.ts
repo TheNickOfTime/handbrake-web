bd5f59c80e83ed0e0a7617b2979015b774e51780
Nick <thenickoftime.x@gmail.com>
2024-08-20 01:17:53 +0000

    feat: display version number in the client's sidebar

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
+import 'dotenv/config';
+import { env } from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,5 +25,8 @@ export default defineConfig({
 	server: {
 		host: '127.0.0.1',
 		port: 5173,
+	},
+	define: {
+		APP_VERSION: JSON.stringify(env.npm_package_version),
 	},
 });
