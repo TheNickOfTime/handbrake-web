66849ed702aa290ce97afa2de81b106265d916c5
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2025-02-21 17:01:53 -0800

    feat: fix sass @import deprecation errors and broken @use instances (#312)
    * feat: adding path alias for styles
    
    * feat: replacing all sass @import statements with @use
    
    * fix: fixing broken @use statements

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 import { env } from 'process';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +13,7 @@ export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
 	resolve: {
 		alias: {
+			'@style': path.resolve(__dirname, './src/style'),
 			components: path.resolve(__dirname, './src/components'),
 			pages: path.resolve(__dirname, './src/pages'),
 			sections: path.resolve(__dirname, './src/sections'),
