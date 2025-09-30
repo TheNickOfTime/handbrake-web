40bb543a03ddb1b4f8fe38fdc5250839a3875617
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2025-09-30 19:36:34 +0000

    feat: updating database-watcher with kysely

--- server/src/scripts/database/database-watcher.ts
+++ server/src/scripts/database/database-watcher.ts
@@ -1,251 +1,163 @@
 import {
-	type WatcherRuleTableType,
-	type WatcherTableType,
+	type AddWatcher,
+	type AddWatcherRule,
+	type UpdateWatcherRule,
 } from '@handbrake-web/shared/types/database';
-import {
-	type WatcherDefinitionObjectType,
-	type WatcherDefinitionType,
-	type WatcherDefinitionWithRulesType,
-	type WatcherRuleDefinitionObjectType,
-	type WatcherRuleDefinitionType,
-} from '@handbrake-web/shared/types/watcher';
+import { jsonArrayFrom } from 'kysely/helpers/postgres';
 import logger from 'logging';
-import { sqliteDatabase } from './database';
+import { database } from './database';
 
-export const watcherTableCreateStatements = [
-	'CREATE TABLE IF NOT EXISTS watchers( \
-		watcher_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
-		watch_path TEXT NOT NULL, \
-		output_path TEXT, \
-		preset_category TEXT NOT NULL, \
-		preset_id TEXT NOT NULL \
-	)',
-	'CREATE TABLE IF NOT EXISTS watcher_rules( \
-		watcher_id INT NOT NULL REFERENCES watchers(watcher_id) ON DELETE CASCADE, \
-		rule_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
-		name TEXT NOT NULL, \
-		mask INTEGER NOT NULL, \
-		base_rule_method INTEGER NOT NULL, \
-		rule_method INTEGER NOT NULL, \
-		comparison_method INTEGER NOT NULL, \
-		comparison TEXT NOT NULL \
-	)',
-];
+const selectFromWatchersDetailed = database
+	.selectFrom('watchers')
+	.select((eb) => [
+		'watcher_id',
+		'watch_path',
+		'output_path',
+		'preset_category',
+		'preset_id',
+		jsonArrayFrom(
+			eb
+				.selectFrom('watcher_rules')
+				.whereRef('watcher_id', '=', 'watchers.watcher_id')
+				.selectAll()
+		).as('rules'),
+	]);
 
-export function GetWatchersFromDatabase() {
+export async function DatabaseGetDetailedWatchers() {
 	try {
-		const watchersStatement = sqliteDatabase.prepare<[], WatcherTableType>(
-			'SELECT * FROM watchers'
-		);
-		const watchersResult = watchersStatement.all();
-
-		const ruleStatement = sqliteDatabase.prepare<{ id: number }, WatcherRuleTableType>(
-			'SELECT * FROM watcher_rules WHERE watcher_id = $id'
-		);
-
-		const rules = Object.fromEntries(
-			watchersResult.map((watcher): [number, WatcherRuleDefinitionObjectType] => [
-				watcher.watcher_id,
-				Object.fromEntries(
-					ruleStatement
-						.all({ id: watcher.watcher_id })
-						.map((rule): [number, WatcherRuleDefinitionType] => [
-							rule.rule_id,
-							{
-								name: rule.name,
-								mask: rule.mask,
-								base_rule_method: rule.base_rule_method,
-								rule_method: rule.rule_method,
-								comparison_method: rule.comparison_method,
-								comparison: rule.comparison,
-							},
-						])
-				),
-			])
-		);
-
-		const watchers: WatcherDefinitionObjectType = Object.fromEntries(
-			watchersResult.map((watcher): [number, WatcherDefinitionWithRulesType] => [
-				watcher.watcher_id,
-				{
-					watch_path: watcher.watch_path,
-					output_path: watcher.output_path,
-					preset_category: watcher.preset_category,
-					preset_id: watcher.preset_id,
-					rules: rules[watcher.watcher_id],
-				},
-			])
-		);
-
+		const watchers = await selectFromWatchersDetailed.execute();
 		return watchers;
 	} catch (err) {
 		logger.error('[server] [database] [error] Could not get watchers from the database.');
-		logger.error(err);
+		throw err;
 	}
 }
 
-export function GetWatcherWithIDFromDatabase(id: number) {
+export async function DatabaseGetDetailedWatcherByID(watcher_id: number) {
 	try {
-		const watcherStatement = sqliteDatabase.prepare<{ id: number }, WatcherTableType>(
-			'SELECT * FROM watchers WHERE watcher_id = $id'
-		);
-		const watcherResult = watcherStatement.get({ id: id });
-
-		if (watcherResult) {
-			const ruleStatement = sqliteDatabase.prepare<{ id: number }, WatcherRuleTableType>(
-				'SELECT * FROM watcher_rules WHERE watcher_id = $id'
-			);
-
-			const rulesResult = Object.fromEntries(
-				ruleStatement.all({ id: id }).map((rule): [number, WatcherRuleDefinitionType] => [
-					rule.rule_id,
-					{
-						name: rule.name,
-						mask: rule.mask,
-						base_rule_method: rule.base_rule_method,
-						rule_method: rule.rule_method,
-						comparison_method: rule.comparison_method,
-						comparison: rule.comparison,
-					},
-				])
-			);
-
-			if (rulesResult) {
-				const watchers: WatcherDefinitionWithRulesType = {
-					watch_path: watcherResult.watch_path,
-					output_path: watcherResult.output_path,
-					preset_category: watcherResult.preset_category,
-					preset_id: watcherResult.preset_id,
-					rules: rulesResult,
-				};
-
-				return watchers;
-			}
-		}
+		const watcher = await selectFromWatchersDetailed
+			.where('watcher_id', '=', watcher_id)
+			.executeTakeFirstOrThrow();
+		return watcher;
 	} catch (err) {
 		logger.error(
-			`[server] [database] [error] Could not get watcher with id '${id}' from the database.`
+			`[server] [database] [error] Could not get watcher with id '${watcher_id}' from the database.`
 		);
-		logger.error(err);
+		throw err;
 	}
 }
 
-export function GetWatcherIDFromRuleIDFromDatabase(id: number) {
+export async function DatabaseGetWatcherIDFromRule(rule_id: number) {
 	try {
-		const idStatement = sqliteDatabase.prepare<{ id: number }, { watcher_id: string }>(
-			'SELECT watcher_id FROM watcher_rules WHERE rule_id = $id'
-		);
-		const idResult = idStatement.get({ id: id });
-		if (idResult) {
-			return parseInt(idResult.watcher_id);
-		}
+		const result = await database
+			.selectFrom('watcher_rules')
+			.where('rule_id', '=', rule_id)
+			.select('watcher_id')
+			.executeTakeFirstOrThrow();
+
+		return result.watcher_id;
 	} catch (err) {
 		logger.error(
-			`[server] [database] [error] Could not get a wacther_id from a rule with '${id}' from the database.`
+			`[server] [database] [error] Could not get a wacther_id from a rule with '${rule_id}' from the database.`
 		);
-		logger.error(err);
+		throw err;
 	}
 }
 
-export function InsertWatcherToDatabase(watcher: WatcherDefinitionType) {
+export async function DatabaseInsertWatcher(watcher: AddWatcher) {
 	try {
-		const insertWatcherStatement = sqliteDatabase.prepare<WatcherDefinitionType>(
-			'INSERT INTO watchers(watch_path, output_path, preset_category, preset_id) VALUES($watch_path, $output_path, $preset_category, $preset_id)'
-		);
-		const insertWatcherResult = insertWatcherStatement.run({
-			watch_path: watcher.watch_path,
-			output_path: watcher.output_path,
-			preset_category: watcher.preset_category,
-			preset_id: watcher.preset_id,
-		});
+		const result = await database
+			.insertInto('watchers')
+			.values(watcher)
+			.executeTakeFirstOrThrow();
 
 		logger.info(
 			`[server] [database] Inserted watcher for '${watcher.watch_path}' into the database.`
 		);
 
-		return insertWatcherResult;
+		return result;
 	} catch (err) {
 		logger.error(
 			`[server] [database] [error] Could not add a watcher for '${watcher.watch_path}' to the database.`
 		);
-		logger.error(err);
+		throw err;
 	}
 }
 
-export function InsertWatcherRuleToDatabase(watcherID: number, rule: WatcherRuleDefinitionType) {
+export async function DatabaseInsertWatcherRule(watcher_id: number, values: AddWatcherRule) {
 	try {
-		const insertRuleStatement = sqliteDatabase.prepare<WatcherRuleTableType>(
-			'INSERT INTO watcher_rules(watcher_id, name, mask, base_rule_method, rule_method, comparison_method, comparison) VALUES($watcher_id, $name, $mask, $base_rule_method, $rule_method, $comparison_method, $comparison)'
-		);
-		const insertRuleResult = insertRuleStatement.run({
-			watcher_id: watcherID,
-			rule_id: 0,
-			...rule,
-		});
+		const result = await database
+			.insertInto('watcher_rules')
+			.values({ watcher_id, ...values })
+			.executeTakeFirstOrThrow();
+
 		logger.info(
-			`[server] [database] Inserted a new rule '${rule.name}' for watcher '${watcherID}' into the database with id '${insertRuleResult.lastInsertRowid}'.`
+			`[server] [database] Inserted a new rule '${values.name}' for watcher '${watcher_id}' into the database with id '${result.insertId}'.`
 		);
-		return insertRuleResult;
+
+		return result;
 	} catch (err) {
 		logger.error(
-			`[server] [database] Could not insert a new rule '${rule.name}' for watcher '${watcherID}' into the database.`
+			`[server] [database] Could not insert a new rule '${values.name}' for watcher '${watcher_id}' into the database.`
 		);
-		logger.error(err);
+		throw err;
 	}
 }
 
-export function UpdateWatcherRuleInDatabase(ruleID: number, rule: WatcherRuleDefinitionType) {
+export async function UpdateWatcherRuleInDatabase(rule_id: number, values: UpdateWatcherRule) {
 	try {
-		const updateStatement = sqliteDatabase.prepare<{ id: number } & WatcherRuleDefinitionType>(
-			'UPDATE watcher_rules SET \
-			name = $name, \
-			mask = $mask, \
-			base_rule_method = $base_rule_method, \
-			rule_method = $rule_method, \
-			comparison_method = $comparison_method, \
-			comparison = $comparison \
-			WHERE rule_id = $id'
-		);
-		const updateResult = updateStatement.run({ id: ruleID, ...rule });
+		const result = database
+			.updateTable('watcher_rules')
+			.set(values)
+			.where('rule_id', '=', rule_id)
+			.executeTakeFirstOrThrow();
+
 		logger.info(
-			`[server] [database] Updated rule '${rule.name}' with id '${ruleID}' in the database.`
+			`[server] [database] Updated rule '${values.name}' with id '${rule_id}' in the database.`
 		);
-		return updateResult;
+
+		return result;
 	} catch (err) {
 		logger.error(
-			`[server] [database] Could not update rule '${rule.name}' with id '${ruleID}' in the database.`
+			`[server] [database] Could not update rule '${values.name}' with id '${rule_id}' in the database.`
 		);
-		logger.error(err);
+		throw err;
 	}
 }
 
-export function RemoveWatcherFromDatabase(id: number) {
+export async function RemoveWatcherFromDatabase(watcher_id: number) {
 	try {
-		const removeStatement = sqliteDatabase.prepare(
-			'DELETE FROM watchers WHERE watcher_id = $id'
+		const result = await database
+			.deleteFrom('watchers')
+			.where('watcher_id', '=', watcher_id)
+			.executeTakeFirstOrThrow();
+
+		logger.info(
+			`[server] [database] Removed watcher with id '${watcher_id}' from the database.`
 		);
-		const removalResult = removeStatement.run({ id: id });
-		logger.info(`[server] [database] Removed watcher with id '${id}' from the database.`);
-		return removalResult;
+
+		return result;
 	} catch (err) {
 		logger.error(
-			`[server] [database] [error] Could not remove a watcher with the id '${id}' from the database.`
+			`[server] [database] [error] Could not remove a watcher with the id '${watcher_id}' from the database.`
 		);
-		logger.error(err);
+		throw err;
 	}
 }
 
-export function RemoveWatcherRuleFromDatabase(id: number) {
+export async function RemoveWatcherRuleFromDatabase(rule_id: number) {
 	try {
-		const removeStatement = sqliteDatabase.prepare(
-			'DELETE FROM watcher_rules WHERE rule_id = $id'
-		);
-		const removalResult = removeStatement.run({ id: id });
-		logger.info(`[server] [database] Removed rule with id '${id}' from the database.`);
-		return removalResult;
+		const result = await database
+			.deleteFrom('watcher_rules')
+			.where('rule_id', '=', rule_id)
+			.executeTakeFirstOrThrow();
+
+		logger.info(`[server] [database] Removed rule with id '${rule_id}' from the database.`);
+
+		return result;
 	} catch (err) {
 		logger.error(
-			`[server] [database] [error] Could not remove a watcher rule with the id '${id}' from the database.`
+			`[server] [database] [error] Could not remove a watcher rule with the id '${rule_id}' from the database.`
 		);
 		logger.error(err);
 	}
