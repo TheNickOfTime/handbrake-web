592b44e1142d4cd67dde4ded887426805b626f7c
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2025-10-06 23:25:32 +0000

    feat: updating the worker to use the improved permissions checking system

---
+++ worker/src/scripts/data.ts
@@ -0,0 +1,35 @@
+import { access, mkdir } from 'fs/promises';
+import path from 'path';
+import { cwd } from 'process';
+import logger from './logging';
+
+export const getDataPath = () => process.env.DATA_PATH || '/tmp/handbrake-web';
+export const getVideoPath = () => process.env.VIDEO_PATH || path.resolve(cwd(), '../video');
+
+export async function InitializeDataPath() {
+	try {
+		await access(getDataPath());
+		if (getDataPath().match(/^\/tmp/)) {
+			logger.info(
+				`The data directory '${getDataPath()}' is a child of '/tmp', but it already exists for some reason?`
+			);
+		} else {
+			logger.info(
+				`The data directory '${getDataPath()}' is not a child of '/tmp' and already exists.`
+			);
+		}
+	} catch (err) {
+		if (getDataPath().match(/^\/tmp/)) {
+			try {
+				logger.info(`Creating the data path at '${getDataPath()}'.`);
+				mkdir(getDataPath());
+			} catch (err) {
+				logger.error(`[error] Could not create the data path at '${getDataPath()}'.`);
+				throw err;
+			}
+		} else {
+			logger.error(`[error] The data path '${getDataPath()}' does not exist.`);
+			throw err;
+		}
+	}
+}
--- worker/src/worker.ts
+++ worker/src/worker.ts
@@ -1,4 +1,13 @@
+import { CheckDirectoryPermissions } from '@handbrake-web/shared/scripts/permissions';
+import { getDataPath, getVideoPath, InitializeDataPath } from 'scripts/data';
+import logger from 'scripts/logging';
+
 async function Worker() {
+	// Check critical permissions
+	await InitializeDataPath();
+	await CheckDirectoryPermissions([getDataPath(), getVideoPath()], logger);
+
+	// Startup only occurs if the previous functions ever finish
 	const startup = await import('./worker-startup');
 	startup.default();
 }
