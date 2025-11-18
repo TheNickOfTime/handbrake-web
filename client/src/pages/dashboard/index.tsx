a35ba7f485d4cc35a89b6ce984a997170bcaf21a
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2025-11-18 20:35:00 +0000

    feat: updating the dashboard to show worker properties

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -4,12 +4,12 @@ import { PrimaryContext } from '~layouts/primary/context';
 import PresetsSection from './sections/presets-section';
 import QueueSection from './sections/queue-section';
 import SummarySection from './sections/summary-section';
-import WorkersSection from './sections/watcher-section';
+import WorkersSection from './sections/workers-section';
 import WatchersSection from './sections/watchers-section';
 import styles from './styles.module.scss';
 
 export default function DashboardSection() {
-	const { socket, queue, queueStatus, presets, connections, watchers } =
+	const { socket, queue, queueStatus, presets, connections, properties, watchers } =
 		useContext(PrimaryContext)!;
 
 	return (
@@ -18,7 +18,7 @@ export default function DashboardSection() {
 			<QueueSection queue={queue} />
 			<PresetsSection presets={presets} />
 			<WatchersSection watchers={watchers} />
-			<WorkersSection queue={queue} workers={connections.workers} />
+			<WorkersSection queue={queue} workers={connections.workers} properties={properties} />
 		</Page>
 	);
 }
--- client/src/pages/dashboard/sections/workers-section/index.tsx
+++ client/src/pages/dashboard/sections/workers-section/index.tsx
@@ -1,5 +1,6 @@
 import { QueueType } from '@handbrake-web/shared/types/queue';
 import { WorkerIDType } from '@handbrake-web/shared/types/socket';
+import { WorkerPropertiesMap } from '@handbrake-web/shared/types/worker';
 import Section from '~components/root/section';
 import DashboardTable from '~pages/dashboard/components/dashboard-table';
 import styles from './styles.module.scss';
@@ -7,16 +8,19 @@ import styles from './styles.module.scss';
 interface Properties {
 	queue: QueueType;
 	workers: WorkerIDType[];
+	properties: WorkerPropertiesMap;
 }
 
-export default function WorkersSection({ queue, workers }: Properties) {
+export default function WorkersSection({ queue, workers, properties }: Properties) {
 	return (
 		<Section className={styles['workers']} heading='Workers' link='/workers'>
 			<DashboardTable>
 				<thead>
 					<tr>
 						<th>Worker ID</th>
-						<th>Connection ID</th>
+						<th>Application Version</th>
+						<th>HandBrake Version</th>
+						<th>Capabilities</th>
 						<th>Status</th>
 					</tr>
 				</thead>
@@ -29,7 +33,21 @@ export default function WorkersSection({ queue, workers }: Properties) {
 						return (
 							<tr key={`worker-${worker}`}>
 								<td>{worker.workerID}</td>
-								<td>{worker.connectionID}</td>
+								<td align='center'>
+									{properties[worker.connectionID].version.application}
+								</td>
+								<td align='center'>
+									{properties[worker.connectionID].version.handbrake}
+								</td>
+								<td align='center'>
+									{Object.entries(properties[worker.connectionID].capabilities)
+										.filter(([_, available]) => available)
+										.map(([capability]) => (
+											<span className={styles['capability']}>
+												{capability.toUpperCase()}
+											</span>
+										))}
+								</td>
 								<td
 									className={`${
 										status == 'Working' ? 'color-blue' : 'color-yellow'
